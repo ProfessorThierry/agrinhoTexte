10fa57ff201aefc2bb9f15a8d3e355e2ef9ceeca
ProfessorThierry <161246655+ProfessorThierry@users.noreply.github.com>
2024-08-06 09:15:03 -0300

    site01

---
+++ node.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    if (window.scrollY > 50) {
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
+});
+
